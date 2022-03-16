d6804d1d3b2a02c5d78c8ddbac3a0b17ac964b76
Expired <imoses2@hotmail.com>
2022-03-16 02:11:38 -0700

    fixes #11 - dashboard is visible

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,19 +5,16 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, (req, res) => {
 	console.log(req.session);
-	console.log('dashboard currently doesnt do anything');
 	
-/*   Post.findAll({
+  Post.findAll({
     where: {
       // use the ID from the session
       user_id: req.session.user_id
     },
     attributes: [
       'id',
-      'post_url',
       'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'created_at'
     ],
     include: [
       {
@@ -37,13 +34,18 @@ router.get('/', withAuth, (req, res) => {
     .then(dbPostData => {
       // serialize data before passing to template
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
+			const username = req.session.username;
+      res.render('dashboard', { 
+				posts, 
+				loggedIn: true,
+				username
+			});
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
- */});
+});
 
 // router.get('/edit/:id', withAuth, (req, res) => {
   // Post.findOne({
