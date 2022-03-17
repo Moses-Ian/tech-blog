34cc3fc5a5957e78461cf73ca9ee3adf6301c660
Expired <imoses2@hotmail.com>
2022-03-16 22:27:12 -0700

    fixes #14 - edit/delete posts

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,2 +1,27 @@
-// fuck it
-//module 14.5
+async function editFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+	const content = document.querySelector('Textarea[name="content"]').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title,
+			content
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
